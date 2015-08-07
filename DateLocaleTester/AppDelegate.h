fc6d9ffb9a9b508d707e6edf1a90511401286375
Eric Freitas <eric@jaybirdsport.com>
2015-08-07 12:13:14 -0500

    Initial Commit

---
+++ DateLocaleTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateLocaleTester
+//
+//  Created by Eric Freitas on 8/7/15.
+//  Copyright © 2015 Eric Freitas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateLocaleTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DateLocaleTester
+//
+//  Created by Eric Freitas on 8/7/15.
+//  Copyright © 2015 Eric Freitas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
