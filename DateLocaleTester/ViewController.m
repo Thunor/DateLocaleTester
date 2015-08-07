897ef3db3bccc47039dea6b90b39c635eb5bdae8
Eric Freitas <eric@jaybirdsport.com>
2015-08-07 14:21:33 -0500

    The complete working app.

--- DateLocaleTester/ViewController.m
+++ DateLocaleTester/ViewController.m
@@ -10,18 +10,78 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UILabel *dateLabel;
+@property (nonatomic, strong) IBOutlet UIButton *localeSelectorButton;
+@property (nonatomic, strong) NSLocale *selectedLocale;
+@property (nonatomic, strong) NSDate *fixedDate;
+
 @end
+
+static const NSString *localeIdentifierUS = @"en-US";
+static const NSString *localeIdentifierAU = @"en-AU";
+
+static const NSString *localeTitleUS = @"US English";
+static const NSString *localeTitleAU = @"AU English";
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.fixedDate = [[self internetInputDateFormatter] dateFromString:@"2015-08-01T12:01:01Z"];
+    
+    [self changeLocaleWithTitle:localeTitleUS andLocale:localeIdentifierUS];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Locale Selector
+
+- (void)changeLocaleWithTitle:(const NSString * __strong)title andLocale:(const NSString * __strong)localeIdentifier {
+    self.selectedLocale = [NSLocale localeWithLocaleIdentifier:(NSString * _Nonnull)localeIdentifier];
+    [self.localeSelectorButton setTitle:(NSString * _Nullable)title forState:UIControlStateNormal];
+    [[self shortDateFormatterLocalized] setLocale:self.selectedLocale];
+    [self.dateLabel setText:[[self shortDateFormatterLocalized] stringFromDate:self.fixedDate]];
 }
+
+- (IBAction)handleSelectorButtonTap:(id)sender {
+    UIAlertController *activitySheet = [UIAlertController alertControllerWithTitle:nil message:NSLocalizedString(@"SELECT LOCALE", @"Select the locale to format the date with.") preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [activitySheet addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", @"The cancel function.") style:UIAlertActionStyleCancel handler:nil]];
+
+    [activitySheet addAction:[UIAlertAction actionWithTitle:(NSString * _Nullable)localeTitleUS style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        if (self.selectedLocale.localeIdentifier != localeIdentifierUS) {
+            [self changeLocaleWithTitle:localeTitleUS andLocale:localeIdentifierUS];
+        }
+    }]];
+    
+    [activitySheet addAction:[UIAlertAction actionWithTitle:(NSString * _Nullable)localeTitleAU style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        if (self.selectedLocale.localeIdentifier != localeIdentifierAU) {
+            [self changeLocaleWithTitle:localeTitleAU andLocale:localeIdentifierAU];
+        }
+    }]];
+    
+    [self presentViewController:activitySheet animated:YES completion:nil];
+}
+
+#pragma mark - Date Formatters
+
+- (NSDateFormatter*)internetInputDateFormatter {
+    static NSDateFormatter *inputFormatter = nil;
+    if (!inputFormatter) {
+        inputFormatter = [[NSDateFormatter alloc] init];
+        [inputFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+    }
+    return inputFormatter;
+}
+
+- (NSDateFormatter*)shortDateFormatterLocalized {
+    static NSDateFormatter *shortFormatter = nil;
+    if (!shortFormatter) {
+        shortFormatter = [[NSDateFormatter alloc] init];
+        [shortFormatter setDateStyle:NSDateFormatterShortStyle];
+    }
+    return shortFormatter;
+}
+
 
 @end
